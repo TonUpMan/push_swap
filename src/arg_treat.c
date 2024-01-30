3b55a8414e8c5e25fe25380d6dbac4dd46866268
tonupman <qdevianne@gmail.com>
2024-01-30 12:04:49 +0100

    changed empty check

--- src/arg_treat.c
+++ src/arg_treat.c
@@ -42,12 +42,14 @@ static void	check_empty(int argc, char **argv, t_stack **a, t_stack **b)
 	while (i <= (argc - 1))
 	{
 		if (!full_space(argv[i]))
-		{
-			ft_putstr_fd("Error\n", 2);
-			ft_free_stacks(a, b);
-			exit(0);
-		}
+			empty++;
 		i++;
+	}
+	if (empty == (argc - 1))
+	{
+		ft_putstr_fd("Error\n", 2);
+		ft_free_stacks(a, b);
+		exit(0);
 	}
 }
 
